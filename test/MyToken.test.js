900510119c9649d4064a4ca7b394d14527fbddff
20120435 <bich1042002@gmail.com>
2024-07-10 14:14:36 +0700

    create simple MyToken (inherit ERC721), deploy contract to blockchain (ganache),

---
+++ test/MyToken.test.js
@@ -0,0 +1,26 @@
+const MyToken = artifacts.require("./MyToken.sol");
+
+require("chai").use(require("chai-as-promised")).should();
+
+contract("My Token", (accounts) => {
+  let token;
+
+  before(async () => {
+    token = await MyToken.deployed();
+  });
+
+  describe("deployment", async () => {
+    it("deploys successfully", async () => {
+      const address = token.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, "");
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
+
+    it("has a name", async () => {
+      const name = await token.name();
+      assert.equal(name, "My Token");
+    });
+  });
+});
